61604052cdad92372aaed24623bb11ed13b33c33
Jochen Manz <manz@arvatis.com>
2016-03-23 18:11:34 +0100

    some basic stuff

---
+++ dev-master/src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ShopwarePlugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        var_dump("test");exit();
+    }
+}
